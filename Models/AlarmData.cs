ac3dff7660d0cdefac6ca09c4c281c0af7116f74
udune <udune@naver.com>
2026-02-27 01:32:03 +0900

    feat: AlarmData 생성자 추가

--- Models/AlarmData.cs
+++ Models/AlarmData.cs
@@ -6,9 +6,19 @@ namespace DigitalTwin.Dashboard.Models
 {
     internal class AlarmData
     {
+        public int Id { get; set; }
         public DateTime Time { get; set; }
         public string Level { get; set; }
         public string Location { get; set; }
         public string Message { get; set; }
+        public bool IsAcknowledged { get; set; }
+
+        public AlarmData()
+        {
+            Time = DateTime.Now;
+            IsAcknowledged = false;
+        }
+
+        public string TimeString => Time.ToString("HH:mm:ss");
     }
 }
