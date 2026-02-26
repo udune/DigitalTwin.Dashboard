2c53b34ad8b9e7f18a1786223a58c29a7f0f5776
udune <udune@naver.com>
2026-02-26 22:53:08 +0900

    feat: AlarmData, AxisData 추가

---
+++ Models/AlarmData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalTwin.Dashboard.Models
+{
+    internal class AlarmData
+    {
+        public DateTime Time { get; set; }
+        public string Level { get; set; }
+        public string Location { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Models/AxisData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalTwin.Dashboard.Models
+{
+    internal class AxisData
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float VelocityX { get; set; }
+        public float VelocityY { get; set; }
+        public float VelocityZ { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
